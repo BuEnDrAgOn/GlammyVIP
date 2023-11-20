c4493f5a50e2e4b48c40afdd03cb9d30aa7fa115
Jose Claudio <jcmm.21@hotmail.com>
2023-11-20 01:12:25 -0600

    baseURL changed

--- src/api/apiURL.js
+++ src/api/apiURL.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 const BereApi = axios.create({
     // baseURL: process.env.VUE_APP_API_URL
-    baseURL: 'http://localhost:3000/api/'
+    baseURL: 'http://137.184.46.65:9393/api/'
 })
 
 BereApi.defaults.headers.common["Content-Type"] = 'application/json';
