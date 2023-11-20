c6856fc2cca7d1939d05a5c4edb8798706be8028
Jose Claudio <jcmm.21@hotmail.com>
2023-11-20 01:56:06 -0600

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 git pull origin develop
 docker compose down
-docker rmi database-frontend
-docker build -t database-frontend .
+docker rmi berefrontend
+docker build -t berefrontend .
 docker compose up -d
